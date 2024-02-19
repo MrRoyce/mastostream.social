b3803be72e17c907809833572a0ac340e1b4160f
RS Harding <royceclrw@gmail.com>
2024-02-18 23:05:55 -0500

    Add count to tags

--- functions/src/collections/tags.js
+++ functions/src/collections/tags.js
@@ -5,18 +5,37 @@ import { FieldValue } from 'firebase-admin/firestore';
 const db = admin.firestore();
 const timestamp = FieldValue.serverTimestamp();
 
-export const addTags = async ({ toot, tags }) => {
-	const { language, tootId } = toot;
+// This will update the timestamp to the latest time
+// that the tag was seen!
+export const addTags = async ({ tags, toot }) => {
+	const { tootId } = toot;
 	tags.forEach(async (tag) => {
-		await db
-			.collection(`tags`)
-			.doc(`${tag.name}`)
-			.set({ language, name: tag.name, timestamp })
-			.catch(async (err) => {
-				const error = `Failed to add tag: ${tag}, ${err.message}`;
-				logger.error(error);
-				throw new Error(error);
-			});
+		// First check if tag exists
+		const tagDoc = db.doc(`tags/${tag.name}`);
+		const tagSnapshot = await tagDoc.get();
+		if (!tagSnapshot.exists) {
+			// add it with count 1
+			await db
+				.collection(`tags`)
+				.doc(`${tag.name}`)
+				.set({ count: 1, name: tag.name, timestamp, lastSeen: timestamp })
+				.catch(async (err) => {
+					const error = `Failed to add tag: ${tag}, ${err.message}`;
+					logger.error(error);
+					throw new Error(error);
+				});
+		} else {
+			// update the count
+			await db
+				.collection(`tags`)
+				.doc(`${tag.name}`)
+				.update({ count: FieldValue.increment(1), lastSeen: timestamp })
+				.catch(async (err) => {
+					const error = `Failed to update tag: ${tag}, ${err.message}`;
+					logger.error(error);
+					throw new Error(error);
+				});
+		}
 
 		await db
 			.collection(`tags/${tag.name}/toots`)
