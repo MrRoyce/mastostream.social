d2dc4b12dfb65f883284243dd4a11f59635bfa15
RS Harding <royceclrw@gmail.com>
2024-02-25 17:12:40 -0500

    Use Promise.all()

--- src/routes/accounts/[acct]/+page.ts
+++ src/routes/accounts/[acct]/+page.ts
@@ -1,17 +1,12 @@
 import type { PageLoad } from './$types';
 import { getDocument, getToots } from '$lib/getCollection';
-import type { DocumentData } from 'firebase/firestore';
-import { formatToot } from '$lib/utils/formatToot';
 
 // Get account and their toots
 export const load: PageLoad = (async ({ params }) => {
-  const entity: DocumentData = await getDocument({ entity: 'accounts', id: params.acct });
+  const [entity, toots] = await Promise.all([
+    await getDocument({ entity: 'accounts', id: params.acct }),
+    await getToots({ entity: 'accounts', id: params.acct, max: 100, orderByField: 'createdAt' })
+  ]);
 
-  const toots: DocumentData[] = await getToots({ entity: 'accounts', id: params.acct, max: 100, orderByField: 'createdAt' })
-
-  const items = toots.map((item) => {
-    return formatToot(item)
-  })
-
-  return { entity: { ...entity }, toots: items };
+  return { entity, toots };
 });
