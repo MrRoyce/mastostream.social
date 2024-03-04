cfe101d6bb601f57908140d5cc7f306a9ee13bc2
RS Harding <royceclrw@gmail.com>
2024-03-04 14:45:03 -0500

    Sort the search by descending createdAt

--- src/routes/search/[type]/[term]/+page.server.ts
+++ src/routes/search/[type]/[term]/+page.server.ts
@@ -2,6 +2,7 @@ import type { PageServerLoad } from './$types';
 import { formatToot } from '$lib/utils/formatToot';
 import { functions } from "$lib/firebase/client";
 import { httpsCallable } from 'firebase/functions';
+import { sortByAttribute } from '$lib/utils/sortObject';
 
 const searchES = httpsCallable(functions, 'searchES');
 
@@ -12,8 +13,12 @@ export const load: PageServerLoad = (async (event) => {
 
   const response = await searchES({ term, type })
   const items = response.data?.data?.hits?.hits.map((item) => {
-    return formatToot(item._source)
+    const formattedToot = formatToot(item._source)
+    console.log('formattedToot', formattedToot)
+    return formattedToot
   })
 
-  return { success: true, toots: items, term };
+  const sortedItems = sortByAttribute(items, 'createdAt')
+
+  return { success: true, toots: sortedItems, term };
 });
