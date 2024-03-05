91202c5fede6582985240c23b815e241351792ef
RS Harding <royceclrw@gmail.com>
2024-03-05 15:51:07 -0500

    Add fediverse logos to page marquee

--- src/lib/components/Footers/FooterPage.svelte
+++ src/lib/components/Footers/FooterPage.svelte
@@ -1,8 +1,59 @@
 <script>
-	// make dynamic date to be added to footer
-	let date = new Date().getFullYear();
+	import { Marquee } from '@selemondev/svelte-marquee';
+	import '@selemondev/svelte-marquee/dist/style.css';
+	import MastodonWhite from '$lib/components/icons/mastodon-wordmark-white-text.svg';
+	import PeertubeBrand from '$lib/components/icons/peertube-brand.svg';
+	import FunkwhaleBrand from '$lib/components/icons/Funkwhalelogo.svg';
+	import PixelfedBrand from '$lib/components/icons/pixelfed-full-color.svg';
+	import MobilizonBrand from '$lib/components/icons/mobilizonlogo.svg';
+	import { Button, Card } from 'flowbite-svelte';
+	import { goto } from '$app/navigation';
+	const sites = [
+		{
+			name: 'Mastodon',
+			text: 'Social Networking thats not for sale',
+			image: MastodonWhite,
+			url: 'https://joinmastodon.org/'
+		},
+		{
+			name: 'Peertube',
+			text: "An alternative to Big Tech's video platforms",
+			image: PeertubeBrand,
+			url: 'https://joinpeertube.org/'
+		},
+		{
+			name: 'Funkwhale',
+			text: 'A platform for all your audio',
+			image: FunkwhaleBrand,
+			url: 'https://www.funkwhale.audio/'
+		},
+		{
+			name: 'Pixelfed',
+			text: 'Explore + Share beautiful photos and videos.',
+			image: PixelfedBrand,
+			url: 'https://pixelfed.org/'
+		},
+		{
+			name: 'Mobilizon',
+			text: 'Gather ⋅ Organize ⋅ Mobilize',
+			image: MobilizonBrand,
+			url: 'https://joinmobilizon.org/en/'
+		}
+	];
 </script>
 
 <main class="container mx-auto">
-	<h2>Page footer</h2>
+	<Marquee pauseOnHover={true} fade={false} reverse={true} class="py-4 motion-reduce:overflow-auto">
+		{#each sites as site}
+			<Button
+				color="dark"
+				class=""
+				on:click={() => {
+					window.open(site.url, '_blank');
+				}}
+				><img class=" w-10 h-auto max-w-xs mr-4" src={site.image} alt="Fediverse site" />
+				{site.text}</Button
+			>
+		{/each}
+	</Marquee>
 </main>
