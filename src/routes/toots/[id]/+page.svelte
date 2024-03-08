99c9e902715bcc128e5091811a65256325442f7b
RS Harding <royceclrw@gmail.com>
2024-03-08 16:54:02 -0500

    Fix toot page formatting

--- src/routes/toots/[id]/+page.svelte
+++ src/routes/toots/[id]/+page.svelte
@@ -67,51 +67,47 @@
 		<div class="container mx-auto my-5 p-5">
 			<div class="md:flex no-wrap md:-mx-2">
 				<!-- Left Side -->
-				<div class="w-full md:w-3/12 md:mx-2 object-top">
+				<div class="w-full md:w-3/12 md:mx-2">
 					<div class="bg-grey-900 shadow-sm border-t-4 border-green-400">
-						<div class="mt-7">
-							<div
-								class="max-w-4xl flex items-top h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0"
-							>
-								<div id="profile" class="w-full shadow-2xl opacity-75 mx-6 lg:mx-0">
-									<div class=" md:p-12 text-center lg:text-left">
-										<p class="text-3xl pb-5">
-											{entity.account?.displayName || entity.account?.display_name || ''}
-										</p>
-										<div class="image overflow-hidden pb-5">
-											<img class="h-auto w-full mx-auto" src={entity.avatar} alt="" />
-										</div>
-										<p class="pb-5">
-											<span class=" truncate">
-												<Button
-													color="dark"
-													class=""
-													on:click={() => {
-														goto(`/accounts/${entity.acct}`);
-													}}>{entity.acct}</Button
-												>
-											</span>
-										</p>
-										<div
-											class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"
-										></div>
-
-										<p class="pt-2 text-base font-bold flex lg:justify-start">
-											<svg
-												class="h-4 fill-current text-green-700 pr-4"
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 20 20"
-											>
-												<path
-													d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"
-												/>
-											</svg>
-											{entity.domain}
-										</p>
-										<p class="pt-2 text-base">
-											{@html accountNote}
-										</p>
+						<div class="max-w-4xl flex items-top h-auto flex-wrap mx-auto lg:my-0">
+							<div id="profile" class="w-full shadow-2xl h-fit mx-6 lg:mx-0">
+								<div class=" md:p-6 text-center lg:text-left">
+									<p class="text-3xl pb-5">
+										{entity.account?.displayName || entity.account?.display_name || ''}
+									</p>
+									<div class="image overflow-hidden pb-5">
+										<img class="h-auto w-full mx-auto" src={entity.avatar} alt="" />
 									</div>
+									<p class="pb-5">
+										<span class=" truncate">
+											<Button
+												color="dark"
+												class=""
+												on:click={() => {
+													goto(`/accounts/${entity.acct}`);
+												}}>{entity.acct}</Button
+											>
+										</span>
+									</p>
+									<div
+										class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"
+									></div>
+
+									<p class="pt-2 text-base font-bold flex lg:justify-start">
+										<svg
+											class="h-4 fill-current text-green-700 pr-4"
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 20 20"
+										>
+											<path
+												d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"
+											/>
+										</svg>
+										{entity.domain}
+									</p>
+									<p class="pt-2 text-base">
+										{@html accountNote}
+									</p>
 								</div>
 							</div>
 						</div>
