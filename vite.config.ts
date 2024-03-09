a389cc4bc8f5da4207143138c59a94b44dadbeda
RS Harding <royceclrw@gmail.com>
2024-03-09 17:41:20 -0500

    Add svelte inspector

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
-
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
