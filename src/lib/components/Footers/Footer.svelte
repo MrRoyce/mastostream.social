ca1a1462be72ea31cc68e06441c1af5e0a487437
RS Harding <royceclrw@gmail.com>
2024-03-10 18:48:49 -0400

    Update footer text

--- src/lib/components/Footers/Footer.svelte
+++ src/lib/components/Footers/Footer.svelte
@@ -18,15 +18,15 @@
 			<div class="grid gap-12 lg:grid-cols-6 lg:gap-24">
 				<div class="col-span-2">
 					<FooterBrand
-						href="https://yourtoots.com"
+						href="https://utoots.com"
 						src="/images/logo.svg"
-						alt="YourToots Logo"
-						name="YourToots"
+						alt="UToots Logo"
+						name="UToots"
 						aClass="flex mb-6"
 					/>
 					<p class="text-gray-600 dark:text-gray-400">
-						YourToots is an ecosystem built on top of Tailwind CSS including a component library,
-						block sections, a Figma design system and other resources.
+						U Toots is a discovery site for Mastodon toots. It respects users privacy (private toots
+						are not searchable) and the standard Mastodon code regarding lawful content.
 					</p>
 				</div>
 				<div>
@@ -34,11 +34,7 @@
 						Resources
 					</h2>
 					<FooterLinkGroup>
-						<FooterLink liClass="mb-4" href="https://flowbite.com/">YourToots</FooterLink>
-						<FooterLink liClass="mb-4" href="https://tailwindcss.com/">Tailwind CSS</FooterLink>
-						<FooterLink liClass="mb-4" href="https://flowbite-svelte.com/"
-							>YourToots-Svelte</FooterLink
-						>
+						<FooterLink liClass="mb-4" href="https://utoots.com/">UToots</FooterLink>
 					</FooterLinkGroup>
 				</div>
 				<div>
@@ -46,10 +42,8 @@
 						HELP & SUPPORT
 					</h2>
 					<FooterLinkGroup>
-						<FooterLink liClass="mb-4" href="https://discord.gg/4eeurUVvTy">Discord</FooterLink>
-						<FooterLink liClass="mb-4" href="https://github.com/themesberg/flowbite-svelte-blocks"
-							>GitHub</FooterLink
-						>
+						<FooterLink liClass="mb-4" href="https://utoots.com">Discord</FooterLink>
+						<FooterLink liClass="mb-4" href="https://utoots.com">GitHub</FooterLink>
 					</FooterLinkGroup>
 				</div>
 				<div>
@@ -57,26 +51,20 @@
 						Follow us
 					</h2>
 					<FooterLinkGroup>
-						<FooterLink liClass="mb-4" href="https://github.com/shinokada/flowbite-svelte-blocks"
-							>Gihub</FooterLink
-						>
-						<FooterLink liClass="mb-4" href="https://discord.gg/4eeurUVvTy">Discord</FooterLink>
+						<FooterLink liClass="mb-4" href="https://utoots.com">Gihub</FooterLink>
+						<FooterLink liClass="mb-4" href="https://utoots.com">Discord</FooterLink>
 					</FooterLinkGroup>
 				</div>
 				<div>
 					<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">Legal</h2>
 					<FooterLinkGroup>
-						<FooterLink
-							liClass="mb-4"
-							href="https://github.com/themesberg/flowbite-svelte-blocks/blob/main/License"
-							>LICENSE</FooterLink
-						>
+						<FooterLink liClass="mb-4" href="https://utoots/com">LICENSE</FooterLink>
 					</FooterLinkGroup>
 				</div>
 			</div>
 			<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 			<div class="sm:flex sm:items-center sm:justify-between">
-				<FooterCopyright href="/" by="Your Toots™" {year} />
+				<FooterCopyright href="/" by="U Toots™" {year} />
 				<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
 					<FooterIcon href="/" class="text-gray-400 hover:text-gray-900" />
 					<FooterIcon href="/" class="text-gray-400 hover:text-gray-900" />
