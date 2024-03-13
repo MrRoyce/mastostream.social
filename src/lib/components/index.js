2a10009159aed78b907014e1114f82f662e14d72
RS Harding <royceclrw@gmail.com>
2024-03-12 21:26:37 -0400

    Add more components to index.js

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,9 +1,23 @@
 import CardLineChart from '$lib/components/Cards/CardLineChart.svelte';
+import CardWithLink from '$lib/components/Cards/Card.svelte';
 import CardStats from '$lib/components/Cards/CardStats.svelte';
 import Footer from '$lib/components/Footers/Footer.svelte';
 import FooterPage from '$lib/components/Footers/FooterPage.svelte';
 import Loading from '$lib/components/Loading/Loading.svelte';
 import OwnersTootTable from '$lib/components/UI/OwnersTootTable.svelte';
+import TootsRadio from '$lib/components/UI/TootsRadio.svelte';
+import TootTable from '$lib/components/UI/TootTable.svelte';
 import WordCloud from '$lib/components/UI/WordCloud.svelte';
 
-export { CardLineChart, CardStats, Footer, FooterPage, Loading, OwnersTootTable, WordCloud };
+export {
+	CardLineChart,
+	CardWithLink,
+	CardStats,
+	Footer,
+	FooterPage,
+	Loading,
+	OwnersTootTable,
+	TootsRadio,
+	TootTable,
+	WordCloud
+};
