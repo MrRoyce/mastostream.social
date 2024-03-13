2f2de407cbba83b724538b11bb69602b6523c7be
RS Harding <royceclrw@gmail.com>
2024-03-12 22:33:32 -0400

    Add more exports to utils

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,7 +1,8 @@
 import { calculateCharts } from '$lib/utils/calculateCharts';
 import { calculateStats } from '$lib/utils/calculateStats';
+import { formatCreatedAt } from '$lib/utils/formatCreatedAt';
 import { formatText } from '$lib/utils/formatText';
 import { formatToot } from '$lib/utils/formatToot';
 import { truncateHTML } from '$lib/utils/truncateHTML';
 
-export { calculateCharts, calculateStats, formatText, formatToot, truncateHTML };
+export { calculateCharts, calculateStats, formatCreatedAt, formatText, formatToot, truncateHTML };
