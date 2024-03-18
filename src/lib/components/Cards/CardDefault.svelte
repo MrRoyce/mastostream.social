fcbed667b33504491dc06a279223418d29936e47
RS Harding <royceclrw@gmail.com>
2024-03-18 18:34:42 -0400

    Add basic card

---
+++ src/lib/components/Cards/CardDefault.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { A, Card } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+
+	export let description: string;
+	export let providerName: string;
+	export let title: string;
+	export let url: string;
+</script>
+
+<Card href={url} target="_blank">
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+	<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+		{description}
+	</p>
+	<A href={url} target="_blank">
+		{providerName}
+		<ArrowRightOutline class="w-3 h-3 ms-2.5" />
+	</A>
+</Card>
