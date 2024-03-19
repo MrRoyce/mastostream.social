64a46e9b7972e819f128095f2005808b8b344f29
RS Harding <royceclrw@gmail.com>
2024-03-19 18:43:33 -0400

    Get latest domain info

--- src/routes/(app)/websites/[domain]/+page.server.ts
+++ src/routes/(app)/websites/[domain]/+page.server.ts
@@ -7,9 +7,28 @@ import { redis } from '$lib/redis/redis';
 const ttl = 600
 let entity
 let toots
+let domain = {}
+
+async function getLatestEntityInfo(fetch, uriWithLookup) {
+  try {
+    const response = await fetch(uriWithLookup)
+    if (!response.ok) {
+      const error = `HTTP error: ${uriWithLookup}, Status: ${response.status}, Text: ${response.statusText}`
+      console.error(error)
+      return null
+    }
+
+    const result = await response.json()
+    return ({ ...result })
+  } catch (err) {
+    const error = `Error calling external API ${uriWithLookup} in getLatestEntityInfo: ${err.message}.`
+    console.error(error, error)
+    return null
+  }
+}
 
 // Get domain and their toots
-export const load: PageServerLoad = (async ({ params, setHeaders }) => {
+export const load: PageServerLoad = (async ({ fetch, params, setHeaders }) => {
   try {
     await redis.connect()
 
@@ -25,6 +44,7 @@ export const load: PageServerLoad = (async ({ params, setHeaders }) => {
 
     if (domainCached && domainTootsCached) {
       console.log(`domainCached, domainTootsCached cached for: ${lowerCase}`)
+
       entity = JSON.parse(domainCached)
       toots = JSON.parse(domainTootsCached)
     } else {
@@ -32,26 +52,59 @@ export const load: PageServerLoad = (async ({ params, setHeaders }) => {
 
       const [entityFromPromise, tootsFromPromise] = await Promise.all([
         await getDocument({ entity: 'domains', id: lowerCase }),
-        await getToots({ entity: 'domains', id: lowerCase, max: 100, orderByField: 'createdAt' })
+        await getToots({
+          entity: 'domains',
+          id: lowerCase,
+          max: 100,
+          orderByField: 'createdAt'
+        })
       ]);
 
       entity = entityFromPromise
       toots = tootsFromPromise
 
-      const items = toots.map((item) => {
-        return formatToot(item)
-      })
+      // Get the latest domain info
+      if (entity && entity.domain && entity.instance) {
 
-      // Store entity in redis
-      await redis.set(redisKeyDomain, JSON.stringify(entity), {
-        EX: ttl
-      })
+        const uriWithLookup = `https://${entity.domain}/api/v1/instance`
 
-      // Store toots for the domain in redis
-      toots = items
-      await redis.set(redisKeyDomainToots, JSON.stringify(items), {
-        EX: ttl
-      })
+        domain = await getLatestEntityInfo(fetch, uriWithLookup)
+
+        console.log(`domain from call to ${uriWithLookup}`, domain)
+
+        entity.instance.stats = domain.stats
+        entity.instance.background_image = domain.background_image
+        entity.instance.thumbnail = domain.thumbnail
+        entity.instance.title = domain.title
+        entity.instance.description = domain.description
+        entity.instance.short_description = domain.short_description
+        entity.instance.email = domain.email
+        entity.instance.approval_required = domain.approval_required
+        entity.instance.languages = domain.languages
+        entity.instance.rules = domain.rules
+        entity.instance.contact_account = domain.contact_account
+
+
+
+        const items = toots.map((item) => {
+          return formatToot(item)
+        })
+
+        // Store entity in redis
+        await redis.set(redisKeyDomain, JSON.stringify(entity), {
+          EX: ttl
+        })
+
+        // Store toots for the domain in redis
+        toots = items
+        await redis.set(redisKeyDomainToots, JSON.stringify(items), {
+          EX: ttl
+        })
+
+      } else {
+        entity = {}
+        console.error(`No entity found for entity: domains and id: ${lowerCase}`)
+      }
 
     }
 
