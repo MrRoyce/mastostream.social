b2832288d1edb9e258534cde18c025dedb18756f
RS Harding <royceclrw@gmail.com>
2024-03-18 22:46:48 -0400

    Make search box smaller

--- src/lib/assets/styles/search.js
+++ src/lib/assets/styles/search.js
@@ -3,5 +3,5 @@ export const searchStyles = {
 	innerDivClass: 'flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-4 p-4 ',
 	searchClass: 'w-full md:w-1/4 relative max-w-96',
 	classInput:
-		'text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2  pl-10 '
+		'text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2  pl-10 '
 };
