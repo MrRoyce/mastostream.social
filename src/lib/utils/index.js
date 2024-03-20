8a6c72f522f0a42e65cd481de4d67c7b69c7c683
RS Harding <royceclrw@gmail.com>
2024-03-20 15:24:20 -0400

    Add other functions

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,17 +1,23 @@
+import { convertToK } from '$lib/utils/convertToK';
 import { calculateCharts } from '$lib/utils/calculateCharts';
 import { calculateStats } from '$lib/utils/calculateStats';
+import { getRandomRange } from '$lib/utils/getRandomRange';
 import { formatCreatedAt } from '$lib/utils/formatCreatedAt';
 import { formatImages } from '$lib/utils/formatImages';
 import { formatText } from '$lib/utils/formatText';
 import { formatToot } from '$lib/utils/formatToot';
 import { truncateHTML } from '$lib/utils/truncateHTML';
+import { summarizeCounts } from '$lib/utils/summarizeCounts';
 
 export {
 	calculateCharts,
 	calculateStats,
+	convertToK,
+	getRandomRange,
 	formatCreatedAt,
 	formatImages,
 	formatText,
 	formatToot,
+	summarizeCounts,
 	truncateHTML
 };
