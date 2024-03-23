0085b8c3a6567840ded7755e4726ac8ada1c321e
RS Harding <royceclrw@gmail.com>
2024-03-23 14:34:20 -0400

    Attempt to fix logout logic

--- src/lib/firebase/handleLogout.ts
+++ src/lib/firebase/handleLogout.ts
@@ -1,6 +1,7 @@
 import { signOut } from 'firebase/auth';
 import { auth } from '$lib/firebase/client';
 import { goto } from '$app/navigation';
+import { authUser } from '$lib/stores';
 
 export const handleLogout = async () => {
   signOut(auth)
@@ -12,7 +13,17 @@ export const handleLogout = async () => {
           // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
         }
       });
-      goto('/');
+
+      const dataToSetToStore = {
+        email: null,
+        displayName: null,
+        uid: null
+      };
+
+      authUser.update((curr: any) => {
+        return { ...curr, ...dataToSetToStore };
+      });
+      goto('/', { invalidateAll: true, replaceState: true });
     })
     .catch((error) => {
       console.error('Error logging out', error);
