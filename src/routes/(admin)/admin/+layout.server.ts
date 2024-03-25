cce52bb256bd3e5c6876daf46a057ef51764e1a1
RS Harding <royceclrw@gmail.com>
2024-03-25 11:55:00 -0400

    Remove extra check for admin user in layout.serer

--- src/routes/(admin)/admin/+layout.server.ts
+++ src/routes/(admin)/admin/+layout.server.ts
@@ -1,15 +1,5 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-
 export const load: LayoutServerLoad = (async ({ locals }) => {
-
-  const { uid, admin } = locals?.user || { uid: null, admin: null }
-
-  if (!uid || !admin) {
-    throw redirect(307, '/')
-  }
-
   return { user: locals.user };
-
 });
