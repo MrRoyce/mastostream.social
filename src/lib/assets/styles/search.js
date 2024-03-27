d5535db8c3478530a7b9f6dc23031ad0e3c1427c
RS Harding <royceclrw@gmail.com>
2024-03-27 11:09:40 -0400

    Remove unused file

--- src/lib/assets/styles/search.js
+++
@@ -1,7 +0,0 @@
-export const searchStyles = {
-	divClass: 'bg-white dark:bg-gray-800 relative shadow-md  ',
-	innerDivClass: 'flex flex-col md:flex-row items-center space-y-3 md:space-y-0 md:space-x-4 p-4 ',
-	searchClass: 'w-full md:w-1/4 relative max-w-96',
-	classInput:
-		'text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2  pl-10 '
-};
