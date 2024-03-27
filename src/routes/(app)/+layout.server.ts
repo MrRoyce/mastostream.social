e6967e945b773f3d46e3e41e28a4d2f33526b9e4
RS Harding <royceclrw@gmail.com>
2024-03-27 13:39:07 -0400

    Remove console.log

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,9 +2,5 @@ import type { LayoutServerLoad } from './$types';
 
 
 export const load: LayoutServerLoad = (async ({ locals }) => {
-
-  console.log('user in (app) +layout.server.ts', locals.user)
-
   return { user: locals.user };
-
 });
