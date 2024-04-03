8295ed59c5f1224d3d4f57f7fb2bd4b69b6b617e
RS Harding <royceclrw@gmail.com>
2024-04-03 10:02:00 -0400

    Initial privacy route

--- src/lib/legal/index.ts
+++ src/lib/legal/index.ts
@@ -1,5 +1,7 @@
+import Privacy from '$lib/legal/Privacy.svelte'
 import TOS from '$lib/legal/TOS.svelte'
 
 export {
+  Privacy,
   TOS
 }
---
+++ src/routes/(legal)/privacy/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { TableWrap } from '$lib/components';
+	import { Privacy } from '$lib/legal';
+</script>
+
+<TableWrap>
+	<Privacy />
+</TableWrap>
