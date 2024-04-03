34ff849e9469973e86e29898ac326b8e8665467b
RS Harding <royceclrw@gmail.com>
2024-04-03 10:04:07 -0400

    Remove wide padding from layout

--- src/lib/components/UI/TableWrap.svelte
+++ src/lib/components/UI/TableWrap.svelte
@@ -4,7 +4,7 @@
 
 <div>
 	<section class="bg-gray-50 dark:bg-gray-900 flex pt-4 mt-4 mx-4 {mb} h-fit">
-		<div class=" px-4 mx-auto lg:px-12 w-full">
+		<div class=" px-4 mx-auto w-full">
 			<slot />
 		</div>
 	</section>
