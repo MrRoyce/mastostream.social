ebbb6560dca494d25eacb7c140094daca952a7b4
RS Harding <royceclrw@gmail.com>
2024-04-02 20:05:07 -0400

    Cookie Consent

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -10,6 +10,8 @@
 	import { browser } from '$app/environment';
 	import { CardLineChart, CardStats, FooterPage, TableWrap, WordCloud } from '$lib/components';
 	import Marqueeck from '@arisbh/marqueeck';
+	import 'vanilla-cookieconsent/dist/cookieconsent.css';
+	import * as CookieConsent from 'vanilla-cookieconsent';
 
 	export let data: PageData;
 	const counts = data.counts;
@@ -21,7 +23,74 @@
 	const { start, end } = getRandomRange(toots?.length, 10);
 	const tootsMarquee = toots?.slice(start, end);
 
-	const stats = calculateStats({ ...latestCounts, counts });
+	if (browser) {
+		CookieConsent.run({
+			categories: {
+				necessary: {
+					enabled: true, // this category is enabled by default
+					readOnly: true // this category cannot be disabled
+				},
+				analytics: {}
+			},
+
+			language: {
+				default: 'en',
+				translations: {
+					en: {
+						consentModal: {
+							title: 'We use cookies',
+							description: 'Cookie modal description',
+							acceptAllBtn: 'Accept all',
+							acceptNecessaryBtn: 'Reject all',
+							showPreferencesBtn: 'Manage Individual preferences'
+						},
+						preferencesModal: {
+							title: 'Manage cookie preferences',
+							acceptAllBtn: 'Accept all',
+							acceptNecessaryBtn: 'Reject all',
+							savePreferencesBtn: 'Accept current selection',
+							closeIconLabel: 'Close modal',
+							sections: [
+								{
+									title: 'Somebody said ... cookies?',
+									description: 'I want one!'
+								},
+								{
+									title: 'Strictly Necessary cookies',
+									description:
+										'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+
+									//this field will generate a toggle linked to the 'necessary' category
+									linkedCategory: 'necessary'
+								},
+								{
+									title: 'Performance and Analytics',
+									description:
+										'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+									linkedCategory: 'analytics'
+								},
+								{
+									title: 'More information',
+									description:
+										'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
+								}
+							]
+						}
+					}
+				}
+			}
+		});
+	}
+
+	const routeTranslations = {
+		toots: $t('pagelinks.toots'),
+		accounts: $t('pagelinks.accounts'),
+		domains: $t('pagelinks.websites'),
+		tags: $t('pagelinks.tags'),
+		languages: $t('pagelinks.languages')
+	};
+	const statsParams = { ...latestCounts, counts, routeTranslations };
+	const stats = calculateStats(statsParams);
 	const charts = calculateCharts(counts);
 
 	if (browser && isSupported()) {
@@ -123,6 +192,7 @@
 
 <style>
 	/* Other styles for your component */
+	@import './cc.css';
 
 	/* Show the slot content only on small screens */
 
