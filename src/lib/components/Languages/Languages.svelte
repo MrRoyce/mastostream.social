e7c75351cdfbf34a83c73c52d521edad066ddb4b
RS Harding <royceclrw@gmail.com>
2024-04-03 11:21:36 -0400

    Auto close Language dropdown on click

--- src/lib/components/Languages/Languages.svelte
+++ src/lib/components/Languages/Languages.svelte
@@ -1,22 +1,23 @@
 <script>
-	import { Dropdown, DropdownItem } from 'flowbite-svelte';
+	import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
 	import { ChevronDown, Globe } from '$lib/components/icons';
 	import { getLanguageList } from '$lib/utils/getLanguage';
 	const languages = getLanguageList();
+	let dropdownOpen = false;
 </script>
 
 <div class="flex">
-	<button
+	<Button
 		id="flags-button"
-		class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+		class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-500 bg-gray-100  hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white "
 		type="button"
 	>
 		<Globe />
 		<span class="pl-4"><ChevronDown /></span>
-	</button>
-	<Dropdown triggeredBy="#flags-button">
+	</Button>
+	<Dropdown triggeredBy="#flags-button" bind:open={dropdownOpen}>
 		{#each languages as language}
-			<DropdownItem class="flex items-center">
+			<DropdownItem class="flex items-center" on:click={() => (dropdownOpen = false)}>
 				<Globe />
 				<span class="text-white pl-4">{language.text}</span>
 			</DropdownItem>
