f5bd7dbb130bec4b1b88a799ee814da22bb0d373
RS Harding <royceclrw@gmail.com>
2024-04-03 10:51:14 -0400

    Remove extra padding on right from SideWrap

--- src/lib/components/UI/SideWrap.svelte
+++ src/lib/components/UI/SideWrap.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div>
-	<section class="bg-gray-50 dark:bg-gray-900 flex pt-4 mt-4 mx-4 {mb} h-fit">
-		<div class="max-w-screen-xl px-4 mx-auto w-full mb-4">
+	<section class="bg-gray-50 dark:bg-gray-900 flex pt-4 mt-4 ml-4 {mb} h-fit">
+		<div class="max-w-screen-xl px-4">
 			<slot />
 		</div>
 	</section>
