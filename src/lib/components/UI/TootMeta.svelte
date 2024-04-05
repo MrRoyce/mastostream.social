e09dab4ac717be8e74d41c5ea8fd9c2eb167dd26
RS Harding <royceclrw@gmail.com>
2024-04-05 19:17:19 -0400

    Add createdAt to TootMeta

--- src/lib/components/UI/TootMeta.svelte
+++ src/lib/components/UI/TootMeta.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import { formatCreatedAt } from '$lib/utils';
+
 	export let counts;
+	export let createdAt: string;
 	const { totalPictures, totalVideos, totalAudio } = counts
 		? counts
 		: { totalPictures: 0, totalVideos: 0, totalAudio: 0 };
 </script>
 
-<div class="grid grid-cols-3 pb-4">
+<div class="grid grid-cols-2 pb-4">
+	<div class="grid col-span-1">
+		{createdAt?.includes('T') ? formatCreatedAt(createdAt) : createdAt}
+	</div>
 	<div class="grid col-span-1 grid-rows-1 grid-flow-col gap-4">
 		<div>
 			<span class="sr-only">Count of pictures</span>
