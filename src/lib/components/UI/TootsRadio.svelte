2608d97a1822d1d6be91a3b71562636f68572358
RS Harding <royceclrw@gmail.com>
2024-04-06 20:29:31 -0400

    Add translations for toot type radio buttons

--- src/lib/components/UI/TootsRadio.svelte
+++ src/lib/components/UI/TootsRadio.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Radio } from 'flowbite-svelte';
+	import { t } from '$lib/translations';
 
 	export let tootType = 'both';
 	export let getTootType = () => {};
@@ -8,15 +9,15 @@
 <div class="grid grid-cols-2">
 	<div class="grid col-span-1 grid-rows-1 grid-flow-col gap-4">
 		<Radio name="hor-list" class="p-2" on:change={getTootType} value={'human'} bind:group={tootType}
-			>Human</Radio
+			>{$t('general.human')}</Radio
 		>
 
 		<Radio name="hor-list" class="p-2" on:change={getTootType} value={'bot'} bind:group={tootType}
-			>Bot</Radio
+			>{$t('general.robot')}</Radio
 		>
 
 		<Radio name="hor-list" class="p-2" on:change={getTootType} value={'both'} bind:group={tootType}
-			>Both</Radio
+			>{$t('general.both')}</Radio
 		>
 	</div>
 </div>
