82f175005e0c9e5f91544c46f95224a1b69310b6
RS Harding <royceclrw@gmail.com>
2024-04-07 17:35:06 -0400

    Temp share icons

--- src/lib/components/UI/TootMeta.svelte
+++ src/lib/components/UI/TootMeta.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
 	import { formatCreatedAt } from '$lib/utils';
+	import {
+		Email,
+		HackerNews,
+		Reddit,
+		LinkedIn,
+		Pinterest,
+		Telegram,
+		Tumblr,
+		Vk,
+		WhatsApp,
+		Xing,
+		Facebook,
+		X,
+		Line
+	} from 'svelte-share-buttons-component';
 
 	export let counts;
 	export let createdAt: string;
+	export let karmaCounts;
+	export let shareContent;
+
 	const { totalPictures, totalVideos, totalAudio } = counts
 		? counts
 		: { totalPictures: 0, totalVideos: 0, totalAudio: 0 };
+
+	const { upCount, downCount, commentsCount } = karmaCounts
+		? karmaCounts
+		: { upCount: 0, downCount: 0, commentsCount: 0 };
+
+	const { url, title, desc } = shareContent ? shareContent : { url: 0, title: 0, desc: 0 };
 </script>
 
+<!-- createdAt -->
+<div class="grid col-span-1">
+	{createdAt?.includes('T') ? formatCreatedAt(createdAt) : createdAt}
+</div>
+
 <div class="grid grid-cols-2 pb-4">
-	<!-- createdAt -->
-	<div class="grid col-span-1">
-		{createdAt?.includes('T') ? formatCreatedAt(createdAt) : createdAt}
-	</div>
 	<!-- mediaAttachmentCounts -->
 	<div class="grid col-span-1 grid-rows-1 grid-flow-col gap-4">
 		<div>
@@ -22,7 +47,7 @@
 		</div>
 		<div>
 			<span class="sr-only">Count of Videos</span>
-			<i class="fa-solid fa-play" style="color: #31c28d;" />
+			<i class="fa-solid fa-play" style="color: #31c48d;" />
 			<span class="pl-1">{totalVideos}</span>
 		</div>
 		<div>
@@ -30,5 +55,32 @@
 			<i class="fa-solid fa-microphone" style="color: #31c48d;" />
 			<span class="pl-1">{totalAudio}</span>
 		</div>
+	</div>
+
+	<!-- Karma counts -->
+	<div class="grid col-span-1 grid-rows-1 grid-flow-col gap-4">
+		<div>
+			<span class="sr-only">Count of up votes</span>
+			<i class="fa-solid fa-thumbs-up" style="color: #31c48d;"></i>
+			<span class="pl-1">{upCount} </span>
+		</div>
+		<div>
+			<span class="sr-only">Count of down votes</span>
+			<i class="fa-solid fa-thumbs-down" style="color: #ea0000;" />
+			<span class="pl-1">{downCount}</span>
+		</div>
+		<div>
+			<span class="sr-only">Count of Comments</span>
+			<i class="fa-solid fa-comment" style="color: #31c48d;" />
+
+			<span class="pl-1">{commentsCount}</span>
+		</div>
+	</div>
+
+	<!-- createdAt -->
+	<div class="grid col-span-1">
+		<Email subject={title} body="{desc} {url}" />
+		<HackerNews class="share-button" {title} {url} />
+		<Reddit class="share-button" {title} {url} />
 	</div>
 </div>
--- src/routes/(app)/toots/+page.svelte
+++ src/routes/(app)/toots/+page.svelte
@@ -162,12 +162,27 @@
 				<div class="show-on-mobile">
 					{#each toots as toot}
 						{@const url = `/toots/${toot.accountId}_${toot.tootId}`}
+						{@const shareContent = {
+							url: `https://utoots.com/toots/${toot.accountId}_${toot.tootId}`,
+							title: 'Test title',
+							desc: 'Test desc'
+						}}
+						{@const karmaCounts = {
+							upCount: toot.upCount || 2,
+							downCount: toot.downCount || 4,
+							commentsCount: toot.commentsCount || 6
+						}}
 						<a href={url}>
 							<TableWrap spacing="px-4">
 								<!-- Contet -->
 								<TootContent {toot} />
 								<!-- Metadata -->
-								<TootMeta createdAt={toot.createdAt} counts={toot.mediaAttachementCounts} />
+								<TootMeta
+									createdAt={toot.createdAt}
+									counts={toot.mediaAttachementCounts}
+									{karmaCounts}
+									{shareContent}
+								/>
 								<!-- Profile -->
 								<div class="md:col-span-1 md:col-start-1 order-last md:order-first">
 									<!-- Account Profile -->
@@ -196,16 +211,7 @@
 														class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"
 													></div>
 
-													<p class="pt-2 text-base font-bold lg:justify-start">
-														<svg
-															class="h-4 fill-current text-green-700 pr-4"
-															xmlns="http://www.w3.org/2000/svg"
-															viewBox="0 0 20 20"
-														>
-															<path
-																d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"
-															/>
-														</svg>
+													<p class="pt-2 text-base font-bold lg:justify-start dark:text-gray-200">
 														{toot.domain}
 													</p>
 												</div>
