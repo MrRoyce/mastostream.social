faf59f9d3e95bc368ca7b2626f2efeb18b8289ab
RS Harding <royceclrw@gmail.com>
2024-04-07 22:26:05 -0400

    Show black background, shrink width of UserSidebar

--- src/lib/components/UserSidebar/UserSidebar.svelte
+++ src/lib/components/UserSidebar/UserSidebar.svelte
@@ -7,9 +7,9 @@
 	$: activeUrl = $page.url.pathname;
 
 	let activeClass =
-		'flex items-center p-2 text-base font-normal text-primary-900 bg-primary-200 dark:bg-gray-900  dark:text-white hover:bg-primary-100 dark:hover:bg-gray-700';
+		'flex items-center p-2 text-base font-normal text-primary-900 bg-primary-200 dark:bg-gray-700  dark:text-white hover:bg-primary-100 dark:hover:bg-gray-700';
 	let nonActiveClass =
-		'flex items-center p-2 text-base font-normal text-green-900  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700';
+		'flex items-center p-2 text-base font-normal text-green-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700';
 
 	export let image: String;
 	export let email: String;
@@ -23,39 +23,43 @@
 </script>
 
 <SideWrap {mb}>
-	<div class="border-2 border-dashed border-gray-300 dark:border-gray-600 mb-4">
-		<Sidebar {activeUrl} {activeClass} {nonActiveClass} asideClass="w-80 ">
-			<SidebarWrapper>
-				<SidebarGroup class="pb-6">
-					<div class="flex items-center justify-center">
-						<SidebarItem>
-							<svelte:fragment slot="icon">
-								{#if image && typeof image === 'string'}
-									<img id="users-picture" height="48" src={image} alt="User" />
-								{:else if email}
-									<i class={'fas fa-user-secret fa-5x'}></i>
-								{/if}
+	<div
+		class="dark:bg-gray-800 border-2 border-dashed border-gray-300 dark:border-gray-600 mb-4 p-4"
+	>
+		<div class="">
+			<Sidebar {activeUrl} {activeClass} {nonActiveClass} asideClass="w-48">
+				<SidebarWrapper class="dark:bg-gray-900 p-4">
+					<SidebarGroup class="">
+						<div class="flex items-center justify-center">
+							<SidebarItem>
+								<svelte:fragment slot="icon">
+									{#if image && typeof image === 'string'}
+										<img id="users-picture" height="48" src={image} alt="User" />
+									{:else if email}
+										<i class={'fas fa-user-secret fa-5x'}></i>
+									{/if}
 
-								<Tooltip triggeredBy="[id^='users-picture']">{email}</Tooltip>
-							</svelte:fragment>
-						</SidebarItem>
-					</div>
-				</SidebarGroup>
-				{#if pageSidebarItems}
-					{#each pageSidebarItems.groups as group}
-						<SidebarGroup class="pb-6">
-							{$t(group.name)}
-							{#each group.items as item}
-								<SidebarItem label={$t(item.label)} href={item.href}>
-									<svelte:fragment slot="icon">
-										<i class={item.icon} />
-									</svelte:fragment>
-								</SidebarItem>
-							{/each}
-						</SidebarGroup>
-					{/each}
-				{/if}
-			</SidebarWrapper>
-		</Sidebar>
+									<Tooltip triggeredBy="[id^='users-picture']">{email}</Tooltip>
+								</svelte:fragment>
+							</SidebarItem>
+						</div>
+					</SidebarGroup>
+					{#if pageSidebarItems}
+						{#each pageSidebarItems.groups as group}
+							<SidebarGroup class="pb-6">
+								{$t(group.name)}
+								{#each group.items as item}
+									<SidebarItem label={$t(item.label)} href={item.href}>
+										<svelte:fragment slot="icon">
+											<i class={item.icon} />
+										</svelte:fragment>
+									</SidebarItem>
+								{/each}
+							</SidebarGroup>
+						{/each}
+					{/if}
+				</SidebarWrapper>
+			</Sidebar>
+		</div>
 	</div>
 </SideWrap>
