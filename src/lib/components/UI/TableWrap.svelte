0a6ddb2a0483581f378d9e2b9b4d88ec37302ef1
RS Harding <royceclrw@gmail.com>
2024-04-08 13:56:05 -0400

    Fix spacing on accounts[acct] page

--- src/lib/components/UI/TableWrap.svelte
+++ src/lib/components/UI/TableWrap.svelte
@@ -1,11 +1,12 @@
 <script>
 	export let mb = '';
 	export let spacing = 'mx-4'; // mx-4
+	export let divContianerPadding = 'px-4';
 </script>
 
 <div>
 	<section class="bg-gray-50 dark:bg-gray-900 flex pt-4 mt-4 {spacing} {mb} h-fit">
-		<div class=" px-4 mx-auto w-full">
+		<div class=" {divContianerPadding} mx-auto w-full">
 			<slot />
 		</div>
 	</section>
