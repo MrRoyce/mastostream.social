9734368a61a14e7432b0784afbe34ab1f021b7b8
RS Harding <royceclrw@gmail.com>
2024-04-07 21:13:33 -0400

    Itermediate changes

--- src/routes/(app)/toots/[id]/+page.svelte
+++ src/routes/(app)/toots/[id]/+page.svelte
@@ -74,244 +74,240 @@
 </script>
 
 {#if entity.acct}
-	<div class="pt-0.5">
-		<TableWrap>
-			<!-- Breadcrumb -->
-			<div class="pl-0 pt-0 pb-4">
-				<Breadcrumb aria-label={$t('aria.breadcrumbLink')}>
-					<BreadcrumbItem href="/" home>{$t('pagelinks.dashboard')}</BreadcrumbItem>
-					<BreadcrumbItem href="/toots">{$t('pagelinks.toots')}</BreadcrumbItem>
-				</Breadcrumb>
-			</div>
-			<div
-				class="dark:bg-gray-800 border-2 border-dashed border-gray-300 dark:border-gray-600 mb-4 p-4"
-			>
-				<div class="dark:bg-gray-900 p-5">
-					<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-						<!-- Toot -->
-						<div class="md:col-span-3 md:col-start-2 order-first md:order-last">
-							<!-- Date and Sensitive Toggle Grid -->
-							<div class="grid col-span-1 md:grid-cols-2">
-								<!-- Created At date -->
-								<div class=" pt-4 col-span-1 md:col-start-1">
-									<div
-										class="flex sm:items-center md:items-start sm:justify-center md:justify-start"
-									>
-										<p class="text-base font-bold flex dark:text-gray-200">
+	<TableWrap>
+		<!-- Breadcrumb -->
+		<div class="pl-0 pt-0 pb-4">
+			<Breadcrumb aria-label={$t('aria.breadcrumbLink')}>
+				<BreadcrumbItem href="/" home>{$t('pagelinks.dashboard')}</BreadcrumbItem>
+				<BreadcrumbItem href="/toots">{$t('pagelinks.toots')}</BreadcrumbItem>
+			</Breadcrumb>
+		</div>
+		<div
+			class="dark:bg-gray-800 border-2 border-dashed border-gray-300 dark:border-gray-600 mb-4 p-4"
+		>
+			<div class="dark:bg-gray-900 p-4">
+				<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+					<!-- Toot -->
+					<div class="md:col-span-3 md:col-start-2 order-first md:order-last">
+						<!-- Date and Sensitive Toggle Grid -->
+						<div class="grid col-span-1 md:grid-cols-2">
+							<!-- Created At date -->
+							<div class=" pt-4 col-span-1 md:col-start-1">
+								<div class="flex sm:items-center md:items-start sm:justify-center md:justify-start">
+									<p class="text-base font-bold flex dark:text-gray-200">
+										<svg
+											class="h-4 fill-current text-green-700 pr-4"
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 20 20"
+										>
+											<path
+												d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"
+											/>
+										</svg>
+										{formatCreatedAt(entity.createdAt)}
+
+										<A target="_blank" href={entity.uri}
+											><ArrowUpRightFromSquareOutline class="w-3 h-3 ms-2.5" />
+										</A>
+									</p>
+								</div>
+							</div>
+
+							<!-- Sensitive Toggle -->
+							<div class="pt-4 col-span-1 md:col-start-2">
+								<div class="flex items-center justify-center">
+									<!-- Sensitive Toggle -->
+									{#if entity.sensitive}
+										<Toggle
+											color="red"
+											checked={false}
+											value="false"
+											on:click={() => {
+												{
+													hideSensitive();
+												}
+											}}>{$t('pagelinks.showSensitive')}</Toggle
+										>
+									{/if}
+								</div>
+							</div>
+						</div>
+						<!-- Reply to if any -->
+						{#if replyTo !== false && typeof replyTo == 'object' && replyTo.acct}
+							<TootTable
+								{tableData}
+								sourceData={replyTo}
+								getData={() => {}}
+								reply={`Replying to this toot`}
+							/>
+						{/if}
+						{#if entity.sensitive && !showSensitive}
+							<div class="pt-10">
+								<Section name="maintenance">
+									<Maintenance>
+										<svelte:fragment slot="h1"
+											>{entity.spoiler_text ||
+												entity.spoilerText ||
+												'Sensitive Content'}</svelte:fragment
+										>
+										<svelte:fragment slot="paragraph"
+											><span class="text-red-700">
+												Toggle the above 'Show Senstive' switch to view.</span
+											></svelte:fragment
+										>
+									</Maintenance>
+								</Section>
+							</div>
+						{:else}
+							<TootContent toot={entity} />
+						{/if}
+
+						{#each images.videos as video}
+							{#if entity.sensitive && !showSensitive}
+								<BlurHash hash={video.blurhash} />
+							{:else}
+								<video
+									controls
+									loop
+									class="h-80"
+									poster={video.previewUrl}
+									muted
+									preload="none"
+									tabindex="0"
+									aria-label={video.description}
+									lang={video.language}
+									volume="1"
+									style="width: 100%;"
+								>
+									<source
+										src={video.src || video.remoteUrl}
+										type={video.type === 'video' || video.src.endsWith('mp4')
+											? 'video/mp4'
+											: video.type}
+									/>
+
+									<!-- Add caption track -->
+									<track
+										kind="captions"
+										label="English"
+										src="captions_en.vtt"
+										srclang={video.language}
+									/>
+
+									<!-- You can add additional caption tracks if needed -->
+									<!-- <track kind="captions" label="Spanish" src="captions_es.vtt" srclang="es"> -->
+
+									Your browser does not support the video tag.
+								</video>
+							{/if}
+						{/each}
+
+						{#if images.pictures}
+							<ImageGallery
+								pictures={images.pictures}
+								sensitive={entity.sensitive}
+								{showSensitive}
+							/>
+						{/if}
+
+						{#each images.audio as audio}
+							<audio controls muted={false} src={audio.src}></audio>
+						{/each}
+
+						{#if card && (card.provider_name || card.title || card.description || card.image || card.url)}
+							<div class="pt-4 justify-center">
+								{#if entity.sensitive && !showSensitive}
+									<br />
+								{:else if card.provider_name === 'YouTube'}
+									<YouTube
+										authorName={card.author_name}
+										cardImage={card.image}
+										imageDescription={card.image_description}
+										title={card.title}
+										url={card.url}
+										videoSource={card.html}
+									/>
+								{:else if card.image}
+									<CardWithImage
+										cardImage={card.image}
+										description={card.description}
+										imageDescription={card.image_description || card.description}
+										providerName={card.provider_name}
+										title={card.title}
+										url={card.url}
+									/>
+								{:else}
+									<CardDefault
+										description={card.description || card.title}
+										providerName={card.provider_name}
+										title={card.title}
+										url={card.url}
+									/>
+								{/if}
+							</div>
+						{/if}
+
+						<!-- Replies to the toot -->
+						{#if replies && replies.length}
+							<TootTable
+								{tableData}
+								sourceData={replies}
+								getData={() => {}}
+								reply={`Replies to this toot`}
+							/>
+						{/if}
+					</div>
+					<!-- Profile -->
+					<div class="md:col-span-1 md:col-start-1 order-last md:order-first">
+						<div class="bg-grey-900 shadow-sm border-t-4 border-green-400">
+							<div class=" items-top h-auto mx-auto lg:my-0">
+								<div id="profile" class="w-full shadow-2xl h-fit mx-0 lg:mx-0">
+									<div class="p-6 text-center lg:text-left">
+										<p class="text-3xl pb-5 text-ellipsis overflow-hidden dark:text-gray-200">
+											{entity.account?.displayName || entity.account?.display_name || ''}
+										</p>
+										<div class="image overflow-hidden pb-5">
+											<img class="h-auto w-full mx-auto" src={entity.avatar} alt="" />
+										</div>
+										<p class="pb-5 text-ellipsis overflow-hidden">
+											<Button
+												color="dark"
+												class=""
+												on:click={() => {
+													goto(`/accounts/${entity.acct}`);
+												}}
+											>
+												<span class="">{entity.acct}</span></Button
+											>
+										</p>
+										<div
+											class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"
+										></div>
+
+										<p class="pt-2 text-base font-bold lg:justify-start dark:text-gray-200">
 											<svg
 												class="h-4 fill-current text-green-700 pr-4"
 												xmlns="http://www.w3.org/2000/svg"
 												viewBox="0 0 20 20"
 											>
 												<path
-													d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"
+													d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"
 												/>
 											</svg>
-											{formatCreatedAt(entity.createdAt)}
-
-											<A target="_blank" href={entity.uri}
-												><ArrowUpRightFromSquareOutline class="w-3 h-3 ms-2.5" />
-											</A>
+											{entity.domain}
 										</p>
-									</div>
-								</div>
-
-								<!-- Sensitive Toggle -->
-								<div class="pt-4 col-span-1 md:col-start-2">
-									<div class="flex items-center justify-center">
-										<!-- Sensitive Toggle -->
-										{#if entity.sensitive}
-											<Toggle
-												color="red"
-												checked={false}
-												value="false"
-												on:click={() => {
-													{
-														hideSensitive();
-													}
-												}}>{$t('pagelinks.showSensitive')}</Toggle
-											>
-										{/if}
-									</div>
-								</div>
-							</div>
-							<!-- Reply to if any -->
-							{#if replyTo !== false && typeof replyTo == 'object' && replyTo.acct}
-								<TootTable
-									{tableData}
-									sourceData={replyTo}
-									getData={() => {}}
-									reply={`Replying to this toot`}
-								/>
-							{/if}
-							{#if entity.sensitive && !showSensitive}
-								<div class="pt-10">
-									<Section name="maintenance">
-										<Maintenance>
-											<svelte:fragment slot="h1"
-												>{entity.spoiler_text ||
-													entity.spoilerText ||
-													'Sensitive Content'}</svelte:fragment
-											>
-											<svelte:fragment slot="paragraph"
-												><span class="text-red-700">
-													Toggle the above 'Show Senstive' switch to view.</span
-												></svelte:fragment
-											>
-										</Maintenance>
-									</Section>
-								</div>
-							{:else}
-								<TootContent toot={entity} />
-							{/if}
-
-							{#each images.videos as video}
-								{#if entity.sensitive && !showSensitive}
-									<BlurHash hash={video.blurhash} />
-								{:else}
-									<video
-										controls
-										loop
-										class="h-80"
-										poster={video.previewUrl}
-										muted
-										preload="none"
-										tabindex="0"
-										aria-label={video.description}
-										lang={video.language}
-										volume="1"
-										style="width: 100%;"
-									>
-										<source
-											src={video.src || video.remoteUrl}
-											type={video.type === 'video' || video.src.endsWith('mp4')
-												? 'video/mp4'
-												: video.type}
-										/>
-
-										<!-- Add caption track -->
-										<track
-											kind="captions"
-											label="English"
-											src="captions_en.vtt"
-											srclang={video.language}
-										/>
-
-										<!-- You can add additional caption tracks if needed -->
-										<!-- <track kind="captions" label="Spanish" src="captions_es.vtt" srclang="es"> -->
-
-										Your browser does not support the video tag.
-									</video>
-								{/if}
-							{/each}
-
-							{#if images.pictures}
-								<ImageGallery
-									pictures={images.pictures}
-									sensitive={entity.sensitive}
-									{showSensitive}
-								/>
-							{/if}
-
-							{#each images.audio as audio}
-								<audio controls muted={false} src={audio.src}></audio>
-							{/each}
-
-							{#if card && (card.provider_name || card.title || card.description || card.image || card.url)}
-								<div class="pt-4 justify-center">
-									{#if entity.sensitive && !showSensitive}
-										<br />
-									{:else if card.provider_name === 'YouTube'}
-										<YouTube
-											authorName={card.author_name}
-											cardImage={card.image}
-											imageDescription={card.image_description}
-											title={card.title}
-											url={card.url}
-											videoSource={card.html}
-										/>
-									{:else if card.image}
-										<CardWithImage
-											cardImage={card.image}
-											description={card.description}
-											imageDescription={card.image_description || card.description}
-											providerName={card.provider_name}
-											title={card.title}
-											url={card.url}
-										/>
-									{:else}
-										<CardDefault
-											description={card.description || card.title}
-											providerName={card.provider_name}
-											title={card.title}
-											url={card.url}
-										/>
-									{/if}
-								</div>
-							{/if}
-
-							<!-- Replies to the toot -->
-							{#if replies && replies.length}
-								<TootTable
-									{tableData}
-									sourceData={replies}
-									getData={() => {}}
-									reply={`Replies to this toot`}
-								/>
-							{/if}
-						</div>
-						<!-- Profile -->
-						<div class="md:col-span-1 md:col-start-1 order-last md:order-first">
-							<div class="bg-grey-900 shadow-sm border-t-4 border-green-400">
-								<div class=" items-top h-auto mx-auto lg:my-0">
-									<div id="profile" class="w-full shadow-2xl h-fit mx-0 lg:mx-0">
-										<div class="p-6 text-center lg:text-left">
-											<p class="text-3xl pb-5 text-ellipsis overflow-hidden dark:text-gray-200">
-												{entity.account?.displayName || entity.account?.display_name || ''}
-											</p>
-											<div class="image overflow-hidden pb-5">
-												<img class="h-auto w-full mx-auto" src={entity.avatar} alt="" />
-											</div>
-											<p class="pb-5 text-ellipsis overflow-hidden">
-												<Button
-													color="dark"
-													class=""
-													on:click={() => {
-														goto(`/accounts/${entity.acct}`);
-													}}
-												>
-													<span class="">{entity.acct}</span></Button
-												>
-											</p>
-											<div
-												class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"
-											></div>
-
-											<p class="pt-2 text-base font-bold lg:justify-start">
-												<svg
-													class="h-4 fill-current text-green-700 pr-4"
-													xmlns="http://www.w3.org/2000/svg"
-													viewBox="0 0 20 20"
-												>
-													<path
-														d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"
-													/>
-												</svg>
-												{entity.domain}
-											</p>
-											<p class="pt-2 text-base text-left overflow-hidden">
-												{@html accountNote}
-											</p>
-										</div>
+										<p class="pt-2 text-base text-left overflow-hidden">
+											{@html accountNote}
+										</p>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div></TableWrap
-		>
-	</div>
+			</div>
+		</div></TableWrap
+	>
 {:else}
 	{#if browser}
 		{redirectToPage()}
