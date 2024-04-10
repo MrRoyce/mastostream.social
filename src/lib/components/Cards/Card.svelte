5209ffaed07de72e07510c7f0d8a4d9c88ab0101
RS Harding <royceclrw@gmail.com>
2024-04-10 19:49:29 -0400

    Remove duplicated text from Card

--- src/lib/components/Cards/Card.svelte
+++ src/lib/components/Cards/Card.svelte
@@ -28,13 +28,10 @@
 			target="_blank"
 			class="flex mx-auto flex-col items-center bg-white border border-gray-200 shadow md:flex-row md:max-w-2xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
 		>
-			<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
-				{title}
-			</h5></a
-		>
-		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-			{imageDescription || description}
-		</p>
+			<p class="mb-3 font-normal text-gray-700 dark:text-gray-300">
+				{imageDescription || description}
+			</p>
+		</a>
 		<A href={url} target="_blank">
 			{providerName}
 			<ArrowRightOutline class="w-3 h-3 ms-2.5" />
