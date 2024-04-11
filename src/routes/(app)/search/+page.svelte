bce77cf8adbc0d106de6a2b018ad9e711f7e115d
RS Harding <royceclrw@gmail.com>
2024-04-11 16:19:08 -0400

    Use the search button instead of radio button for search

--- src/routes/(app)/search/+page.svelte
+++ src/routes/(app)/search/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-	import { Breadcrumb, BreadcrumbItem, Radio } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Button, Heading, Radio } from 'flowbite-svelte';
 	import { page } from '$app/stores';
 	import { getAnalytics, logEvent } from 'firebase/analytics';
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import { TableWrap } from '$lib/components';
 	import { t } from '$lib/translations';
+
+	let searchTerm = '';
+	let selected = 'toot';
 
 	if (browser) {
 		const analytics = getAnalytics();
@@ -14,7 +17,10 @@
 		});
 	}
 
-	let searchTerm = '';
+	function onChange(thisSelection) {
+		selected = thisSelection;
+	}
+
 	$: activeUrl = $page.url?.pathname;
 </script>
 
@@ -27,9 +33,14 @@
 			</Breadcrumb>
 		</div>
 		<div class="dark:bg-gray-800 border-2 border-dashed border-gray-300 dark:border-gray-600 mb-4">
-			<div class=" mx-auto my-5 p-5">
+			<div class=" mx-auto mb-5 p-4">
+				<div class="col-span-2 mb-6">
+					<Heading class="text-xl md:text-3xl lg:text-5xl  dark:text-gray-200"
+						>Search in Toots, Websites, or Accounts</Heading
+					>
+				</div>
 				<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-					<div class="md:col-span-2 md:col-start-1 order-first">
+					<div class="md:col-span-2 md:col-start-1 order-first mb-6">
 						<form class="flex items-left">
 							<label for="entity-search" class="sr-only">Search</label>
 							<div class="relative w-full">
@@ -46,46 +57,52 @@
 									required
 								/>
 							</div>
+							<Button
+								color="green"
+								on:click={() => goto(`/search/${selected}/${searchTerm}`)}
+								class="rounded-none py-2.5 px-3.5  ms-2 text-sm font-medium dark:text-gray-200"
+							>
+								<svg
+									class="w-4 h-4"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 20 20"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+									/>
+								</svg>
+								<span class="sr-only">Search</span>
+							</Button>
 						</form>
 					</div>
 					<div class="md:col-span-2 md:col-start-3 order-last">
-						<ul
-							class=" py-0.5 items-center text-sm font-medium text-gray-900 bg-white border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
-						>
-							<li
-								class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600"
+						<div class="grid col-span-1 grid-rows-1 grid-flow-col gap-4">
+							<Radio
+								name="search-list"
+								bind:group={searchTerm}
+								class="p-2"
+								on:click={() => onChange('website')}>Website</Radio
 							>
-								<div class="flex items-center ps-3">
-									<Radio
-										name="search-list"
-										class="p-2"
-										on:click={() => goto(`/search/toot/${searchTerm}`)}>Toot Content</Radio
-									>
-								</div>
-							</li>
-							<li
-								class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600"
+
+							<Radio
+								name="search-list"
+								bind:group={searchTerm}
+								class="p-2"
+								on:click={() => onChange('account')}>Account</Radio
 							>
-								<div class="flex items-center ps-3">
-									<Radio
-										name="search-list"
-										class="p-2"
-										on:click={() => goto(`/search/website/${searchTerm}`)}>Website</Radio
-									>
-								</div>
-							</li>
-							<li
-								class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600"
+							<Radio
+								name="search-list"
+								bind:group={searchTerm}
+								class="p-2"
+								on:click={() => onChange('toot')}>Toot Content</Radio
 							>
-								<div class="flex items-center ps-3">
-									<Radio
-										name="search-list"
-										class="p-2"
-										on:click={() => goto(`/search/account/${searchTerm}`)}>Account Name</Radio
-									>
-								</div>
-							</li>
-						</ul>
+						</div>
 					</div>
 				</div>
 			</div>
