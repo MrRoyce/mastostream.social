0d1e2d4c3a1a2253e1e9458e977f62e24db94508
RS Harding <royceclrw@gmail.com>
2024-04-10 21:41:40 -0400

    Use TableWrap instead of Section

--- src/lib/components/UI/Page404.svelte
+++ src/lib/components/UI/Page404.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { redirectPage } from '$lib/utils/redirectPage';
 	import { Button } from 'flowbite-svelte';
-	import { Page404 } from 'flowbite-svelte-blocks';
+	import { Page404, Section } from 'flowbite-svelte-blocks';
+	import { TableWrap } from '$lib/components';
 
 	export let route: string;
 
@@ -12,16 +13,18 @@
 	redirectToPage();
 </script>
 
-<Page404>
-	<svelte:fragment slot="h1">404</svelte:fragment>
-	<svelte:fragment slot="paragraph">
-		<p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
-			Something's missing.
-		</p>
-		<p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-			Sorry, we can't find that page. You'll find lots to explore on the /{route} page. Please click
-			here if you are not redirected
-		</p>
-		<Button href={`/${route}`} size="lg" color="red">Back to /{route}</Button>
-	</svelte:fragment>
-</Page404>
+<TableWrap divContainerPadding="px-48">
+	<Page404>
+		<svelte:fragment slot="h1">404</svelte:fragment>
+		<svelte:fragment slot="paragraph">
+			<p class="mb-4 text-3xl tracking-tight font-bold text-gray-200 md:text-4xl">
+				Something's missing.
+			</p>
+			<p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-200">
+				Sorry, we can't find that page. You'll find lots to explore on the /{route} page. Please click
+				here if you are not redirected
+			</p>
+			<Button href={`/${route}`} size="lg" color="red">Back to /{route}</Button>
+		</svelte:fragment>
+	</Page404>
+</TableWrap>
