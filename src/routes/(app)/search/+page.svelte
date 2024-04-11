1370fa5ee62f4550e1db436eb2b662cdea99de5c
RS Harding <royceclrw@gmail.com>
2024-04-11 16:41:56 -0400

    Add adult content filter to search

--- src/routes/(app)/search/+page.svelte
+++ src/routes/(app)/search/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Breadcrumb, BreadcrumbItem, Button, Heading, Radio } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Button, Heading, Modal, Radio } from 'flowbite-svelte';
 	import { page } from '$app/stores';
 	import { getAnalytics, logEvent } from 'firebase/analytics';
 	import { browser } from '$app/environment';
@@ -10,6 +10,7 @@
 
 	let searchTerm = '';
 	let selected = 'toot';
+	let adultModal = false;
 
 	const badWords = getBadWords();
 
@@ -25,9 +26,22 @@
 		selected = thisSelection;
 	}
 
+	const ageConfirmed = () => {
+		goto(`/search/${selected}/${searchTerm}`);
+	};
+
+	const ageDecline = () => {
+		// reset form
+	};
+
 	function searchText() {
 		// console.log(`Will search for ${searchTerm} in ${selected}`);
 		const adultContent = hasAdultContent(searchTerm, badWords);
+		if (adultContent) {
+			adultModal = true;
+		} else {
+			ageConfirmed();
+		}
 		console.log('adultContent', adultContent);
 		//goto(`/search/${selected}/${searchTerm}`);
 	}
@@ -120,3 +134,26 @@
 		</div>
 	</TableWrap>
 </div>
+
+<Modal title="" bind:open={adultModal} autoclose size="sm" class="w-full">
+	<svg
+		class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
+		aria-hidden="true"
+		fill="currentColor"
+		viewBox="0 0 20 20"
+		xmlns="http://www.w3.org/2000/svg"
+		><path
+			fill-rule="evenodd"
+			d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+			clip-rule="evenodd"
+		/></svg
+	>
+	<p class="mb-4 text-gray-500 dark:text-gray-300 text-center">
+		This is adult restricted content. Please confirm that you are of legal age to view this content
+		in your location.?
+	</p>
+	<div class="flex justify-center items-center space-x-4">
+		<Button color="green" on:click={ageDecline}>No, cancel</Button>
+		<Button color="red" on:click={ageConfirmed}>Yes, I am of legal age</Button>
+	</div>
+</Modal>
