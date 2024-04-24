c165c659757142ef8d35dc33fac5159e57b8f083
RS Harding <royceclrw@gmail.com>
2024-04-24 17:41:53 -0400

    Use spread operator on return

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,5 @@
 export async function load({ locals }) {
-
   return {
-    user: locals.user && {
-      admin: locals.user.admin,
-      displayName: locals.user.displayName,
-      email: locals.user.email,
-      picture: locals.user.picture,
-      uid: locals.user.uid
-    }
+    ...locals
   };
 }
