0c1f4d0d545a43cf3461e4d82d8edb665ab92da3
RS Harding <royceclrw@gmail.com>
2024-04-24 18:01:17 -0400

    Remove ssr and prerender false

--- src/routes/(app)/toots/+page.ts
+++ src/routes/(app)/toots/+page.ts
@@ -1,8 +1,5 @@
 import type { PageLoad } from './$types';
 
-export const ssr = false;
-export const prerender = false;
-
 export const load: PageLoad = (async ({ url, data }) => {
   const tootType = url.searchParams.get('type') ?? 'both'
   return {
