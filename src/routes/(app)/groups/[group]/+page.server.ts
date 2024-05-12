222ef724f08213c5dc9b7c737ddbcd415ed10a4f
RS Harding <royceclrw@gmail.com>
2024-05-12 18:04:12 -0400

    Get the user record to get acct

--- src/routes/(app)/groups/[group]/+page.server.ts
+++ src/routes/(app)/groups/[group]/+page.server.ts
@@ -1,7 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { getDocument } from '$lib/getCollection';
 
-const maxParamLength = 25
+const maxParamLength = 50
 
 export const load: PageServerLoad = (async ({ params, locals }) => {
 
@@ -16,7 +17,10 @@ export const load: PageServerLoad = (async ({ params, locals }) => {
     throw redirect(307, '/')
   }
 
+  const userDocument = (user.uid) ? await getDocument({ entity: 'users', id: user.uid }) : {}
+
   return {
+    acct: userDocument?.acct || '',
     user,
     group: params.group
   };
