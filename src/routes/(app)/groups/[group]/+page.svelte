743735b0b0112e8c1c2862ecd5538646ce7ef558
RS Harding <royceclrw@gmail.com>
2024-05-22 13:17:22 -0400

    Subscribe to stores - show messages

--- src/routes/(app)/groups/[group]/+page.svelte
+++ src/routes/(app)/groups/[group]/+page.svelte
@@ -26,13 +26,28 @@
 		createUser({ acct: data.entity?.acct || 'Anonymous', group });
 	});
 
+	chatMessagesStore.subscribe(() => {
+		// Scroll down
+		const chatMessages = document.getElementById('chat-messages');
+		const messageInput = document.getElementById('messageInput');
+		if (chatMessages) {
+			//chatMessages.scroll({ top: chatMessages.scrollHeight, behavior: 'smooth' });
+			//chatMessages.scrollTop = chatMessages.scrollHeight;
+			setTimeout(() => {
+				chatMessages.scrollTop = chatMessages.scrollHeight;
+				if (messageInput) {
+					messageInput.focus();
+				}
+			}, 0);
+		}
+	});
+
 	function submitMessage() {
-		if (user.uid) {
+		if (user.uid && messageInput) {
 			sendMessage(messageInput, data.entity?.acct || 'Anonymous');
-		} else {
-			console.log('No user.uid!');
 		}
 
+		// Clear the message
 		messageInput = '';
 	}
 </script>
@@ -45,116 +60,135 @@
 			<div class="grid grid-cols-2 gap-4">
 				<Heading tag="h3">{group} Group</Heading>
 			</div>
-			<div class="mt-4">
-				<div class="grid grid-cols-12 gap-4">
-					<!-- Groups -->
-					<div class="col-span-2">
-						<Table
-							name="advancedTable"
-							classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
-							hoverable={true}
-						>
-							<TableHead
-								><TableHeadCell class="text-center " padding="px-4 py-3" scope="col"
-									>Rooms</TableHeadCell
-								></TableHead
+			<div class="overflow-y-scroll">
+				<div class="mt-4">
+					<div class="grid grid-cols-12 gap-4">
+						<div class="col-span-2">
+							<Table
+								name="advancedTable"
+								classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
+								hoverable={true}
 							>
-							<TableBody>
-								<!-- List the rooms -->
-								{#each $chatRoomsStore as chatRoom}
-									<TableBodyRow class="border-none cursor-pointer">
-										<TableBodyCell class="pl-4">
-											{chatRoom.name}
-										</TableBodyCell>
-									</TableBodyRow>
-								{/each}
-							</TableBody>
-						</Table>
-					</div>
+								<TableHead
+									><TableHeadCell class="text-center " padding="px-4 py-3" scope="col"
+										>Rooms</TableHeadCell
+									></TableHead
+								>
+							</Table>
+						</div>
+						<div class="col-span-8">
+							<Table
+								name="advancedTable"
+								classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
+								hoverable={true}
+							>
+								<TableHead
+									><TableHeadCell class="text-center " padding="px-4 py-3" scope="col"
+										>Messages</TableHeadCell
+									></TableHead
+								>
+							</Table>
+						</div>
+						<div class="col-span-2">
+							<Table
+								name="advancedTable"
+								classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
+								hoverable={true}
+							>
+								<TableHead
+									><TableHeadCell class="text-center " padding="px-4 py-3" scope="col"
+										>Users</TableHeadCell
+									></TableHead
+								>
+							</Table>
+						</div>
 
-					<!-- Messages -->
-					<div class="col-span-8">
-						<Table
-							name="advancedTable"
-							classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
-							hoverable={true}
-						>
-							<TableHead
-								><TableHeadCell class="text-center " padding="px-4 py-3" scope="col"
-									>Messages</TableHeadCell
-								></TableHead
+						<!-- Groups -->
+						<div class="col-span-2">
+							<Table
+								name="advancedTable"
+								classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
+								hoverable={true}
 							>
-							<TableBody>
-								<!-- List the messages -->
-								{#each $chatMessagesStore.reverse() as chatMessage}
-									<TableBodyRow class="border-none cursor-pointer">
-										<TableBodyCell class="pl-4">
-											({chatMessage.userName}) - {chatMessage.content}
-										</TableBodyCell>
-									</TableBodyRow>
-								{/each}
-							</TableBody>
-						</Table>
-					</div>
+								<TableBody>
+									<!-- List the rooms -->
+									{#each $chatRoomsStore as chatRoom}
+										<TableBodyRow class="border-none cursor-pointer">
+											<TableBodyCell class="pl-4">
+												{chatRoom.name}
+											</TableBodyCell>
+										</TableBodyRow>
+									{/each}
+								</TableBody>
+							</Table>
+						</div>
 
-					<!-- Users -->
-					<div class="col-span-2">
-						<Table
-							name="advancedTable"
-							classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
-							hoverable={true}
-						>
-							<TableHead
-								><TableHeadCell class="text-center " padding="px-4 py-3" scope="col"
-									>Users</TableHeadCell
-								></TableHead
+						<!-- Messages -->
+						<div class="col-span-8">
+							<Table
+								name="advancedTable"
+								classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
+								hoverable={true}
 							>
-							<TableBody>
-								<!-- List the users -->
-								{#each $chatUsersStore as chatUser}
-									<TableBodyRow class="border-none cursor-pointer">
-										<TableBodyCell class="pl-4" on:click={() => goto(`/accounts/${acct}`)}>
-											{chatUser.userName}
-										</TableBodyCell>
-									</TableBodyRow>
-								{/each}
-							</TableBody>
-						</Table>
-					</div>
-				</div>
-				<div class="grid grid-cols-12 gap-4">
-					<div class="col-span-2"></div>
-					<div class="col-span-8">
-						<form>
-							<div class="grid grid-cols-3 gap-4">
-								<Input
-									type="text"
-									class="col-span-2"
-									name="messageInput"
-									bind:value={messageInput}
-								/>
-								<div class="col-span-1">
-									<Button on:click={submitMessage}>
-										<svg
-											class=" -ml-1 w-6 h-6"
-											fill="currentColor"
-											viewBox="0 0 20 20"
-											xmlns="http://www.w3.org/2000/svg"
-											><path
-												fill-rule="evenodd"
-												d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-												clip-rule="evenodd"
-											/></svg
-										>
-										Send Message
-									</Button>
+								<div id="chat-messages" class="pb-2 h-96 overflow-y-scroll">
+									<TableBody>
+										<!-- List the messages -->
+										{#each $chatMessagesStore as chatMessage}
+											<TableBodyRow class="border-none cursor-pointer">
+												<TableBodyCell class="pl-4">
+													({chatMessage.userName}) - {chatMessage.content}
+												</TableBodyCell>
+											</TableBodyRow>
+										{/each}
+									</TableBody>
 								</div>
-							</div>
-						</form>
+							</Table>
+						</div>
+
+						<!-- Users -->
+						<div class="col-span-2">
+							<Table
+								name="advancedTable"
+								classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
+								hoverable={true}
+							>
+								<TableBody>
+									<!-- List the users -->
+									{#each $chatUsersStore as chatUser}
+										<TableBodyRow class="border-none cursor-pointer">
+											<TableBodyCell class="pl-4" on:click={() => goto(`/accounts/${acct}`)}>
+												{chatUser.userName}
+											</TableBodyCell>
+										</TableBodyRow>
+									{/each}
+								</TableBody>
+							</Table>
+						</div>
 					</div>
-					<div class="col-span-2"></div>
+
+					<!-- Message input and Send Button -->
+					<div class="grid grid-cols-12 pt-4">
+						<div class="col-span-2"></div>
+						<div class="col-span-8">
+							<form>
+								<div class="grid grid-cols-12 gap-4">
+									<Input
+										type="text"
+										class="col-span-10 rounded-none"
+										name="messageInput"
+										id="messageInput"
+										bind:value={messageInput}
+									/>
+									<div class="col-span-2">
+										<Button on:click={submitMessage} class="rounded-none">Send Message</Button>
+									</div>
+								</div>
+							</form>
+						</div>
+						<div class="col-span-2"></div>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</TableWrap>
+	</div></TableWrap
+>
