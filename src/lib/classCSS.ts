5f6d29aa12074415342d89558a84519ee45d84d9
RS Harding <royceclrw@gmail.com>
2024-06-13 15:40:43 -0400

    Move class to file

---
+++ src/lib/classCSS.ts
@@ -0,0 +1,6 @@
+export const activeClass =
+  'flex items-center p-2 text-base font-normal text-primary-900 bg-primary-200 dark:bg-gray-700 dark:text-white hover:bg-primary-100 dark:hover:bg-gray-700';
+export const nonActiveClass =
+  'flex items-center p-2 text-base font-normal text-green-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700';
+export const ownTextClass = 'border-none cursor-pointer text-right';
+export const sentTextClass = 'border-none cursor-pointer'
