1e983644be736d31ee3843bf91d9a2779d0d7fc7
RS Harding <royceclrw@gmail.com>
2024-06-28 19:27:38 -0400

    Remove user when disconnected

--- src/routes/(app)/messages/+page.svelte
+++ src/routes/(app)/messages/+page.svelte
@@ -237,8 +237,10 @@
 					}
 				);
 
-				socket.on('user disconnected', (user) => {
-					console.log('user disconnected', user);
+				socket.on('userDisconnected', (uid) => {
+					chatUsersStore.update((users) => users.filter((user) => user.userID !== uid));
+
+					console.log('userDisconnected', uid);
 				});
 
 				socket.on('updateUsers', (updatedUsers) => {
@@ -278,7 +280,7 @@
 								}
 							}
 						} else {
-							console.error('invalid user', user);
+							console.error('Invalid user in updateUsers', user);
 						}
 					}
 
@@ -424,61 +426,132 @@
 							classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5"
 							hoverable={true}
 						>
-							<div id="chat-users" class="pb-2 h-96 overflow-y-scroll">
-								<TableBody>
+							<div class="pb-2 overflow-y-scroll w-full">
+								<TableBody tableBodyClass="w-full">
 									<!-- List the users -->
 									{#each userList as chatUser}
 										{#if acct !== chatUser.username}
-											<!-- content here -->
+											<div class="pt-4 w-full">
+												<TableBodyRow scope="col" class="border-none cursor-pointer mt-4 w-full">
+													<TableBodyCell
+														scope="col"
+														class={userNameClicked === chatUser.username
+															? activeClass
+															: nonActiveClass}
+														on:click={() => userClicked(chatUser)}
+													>
+														<!-- User Profile Tab Card -->
+														<div class="flex flex-row border-gray-200/80 p-6 w-full">
+															<!-- Avatar Container -->
 
-											<TableBodyRow class="border-none cursor-pointer">
-												<TableBodyCell
-													class={userNameClicked === chatUser.username
-														? activeClass
-														: nonActiveClass}
-													on:click={() => userClicked(chatUser)}
-												>
-													<Card padding="lg" size="sm">
-														<div class="flex justify-end">
-															<DotsHorizontalOutline />
-															<Dropdown class="w-36">
-																<DropdownItem
-																	on:click={() => console.log(`Block ${chatUser.username} `)}
-																	}
-																>
-																	Block</DropdownItem
-																>
-																<DropdownItem
-																	on:click={() => console.log(`Report ${chatUser.username} `)}
-																	}
-																>
-																	Report</DropdownItem
-																>
-															</Dropdown>
-														</div>
-														<div class="flex items-center space-x-4 rtl:space-x-reverse">
+															<!-- User Avatar -->
 															<Avatar
+																rounded
 																src={chatUser.photoURL}
 																alt={chatUser.photoURL}
 																class="flex-shrink-0"
+																dot={{
+																	placement: 'bottom-right',
+																	color: chatUser.connected ? 'green' : 'red'
+																}}
 															/>
-															{#if acct == chatUser.username}
-																{''}
-															{:else}
-																{chatUser.newMessagesCount || ''}
-															{/if}
 
-															{#if chatUser.connected}
-																{'✅'}
-															{:else}
-																{'🚫'}
-															{/if}
+															<!-- Meta Body -->
+															<div class="flex flex-col px-6">
+																<!-- Username Container -->
+																<div class="flex h-8 flex-row">
+																	<!-- Username -->
+																	<h2 class="text-lg font-semibold">{chatUser.username}</h2>
 
-															{chatUser.username}
-														</div></Card
+																	<!-- User Verified -->
+																	<svg
+																		class="my-auto ml-2 h-5 fill-blue-400"
+																		xmlns="http://www.w3.org/2000/svg"
+																		xmlns:xlink="http://www.w3.org/1999/xlink"
+																		version="1.1"
+																		width="24"
+																		height="24"
+																		viewBox="0 0 24 24"
+																	>
+																		<path
+																			d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z"
+																		/>
+																	</svg>
+																</div>
+
+																<!-- Meta Badges -->
+																<div class="my-2 flex flex-row space-x-2">
+																	<!-- Badge Role -->
+																	<div class="flex flex-row">
+																		<svg
+																			class="mr-2 h-4 w-4 fill-gray-500/80"
+																			xmlns="http://www.w3.org/2000/svg"
+																			xmlns:xlink="http://www.w3.org/1999/xlink"
+																			version="1.1"
+																			width="24"
+																			height="24"
+																			viewBox="0 0 24 24"
+																		>
+																			<path
+																				d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7.07,18.28C7.5,17.38 10.12,16.5 12,16.5C13.88,16.5 16.5,17.38 16.93,18.28C15.57,19.36 13.86,20 12,20C10.14,20 8.43,19.36 7.07,18.28M18.36,16.83C16.93,15.09 13.46,14.5 12,14.5C10.54,14.5 7.07,15.09 5.64,16.83C4.62,15.5 4,13.82 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,13.82 19.38,15.5 18.36,16.83M12,6C10.06,6 8.5,7.56 8.5,9.5C8.5,11.44 10.06,13 12,13C13.94,13 15.5,11.44 15.5,9.5C15.5,7.56 13.94,6 12,6M12,11A1.5,1.5 0 0,1 10.5,9.5A1.5,1.5 0 0,1 12,8A1.5,1.5 0 0,1 13.5,9.5A1.5,1.5 0 0,1 12,11Z"
+																			/>
+																		</svg>
+
+																		<div class="text-xs text-gray-400/80 hover:text-gray-400">
+																			{chatUser.newMessagesCount || '0'} New Comments
+																		</div>
+																	</div>
+																</div>
+															</div>
+
+															<!-- Right Actions Container -->
+															<div class="w-100 flex flex-grow flex-col items-end justify-start">
+																<div class="flex flex-row space-x-3">
+																	<!-- Follow Button -->
+																	<button
+																		class="flex rounded-md bg-blue-500 py-2 px-4 text-white transition-all duration-150 ease-in-out hover:bg-blue-600"
+																	>
+																		<svg
+																			class="mr-2 fill-current"
+																			xmlns="http://www.w3.org/2000/svg"
+																			xmlns:xlink="http://www.w3.org/1999/xlink"
+																			version="1.1"
+																			width="24"
+																			height="24"
+																			viewBox="0 0 24 24"
+																		>
+																			<path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg
+																		>
+
+																		Follow
+																	</button>
+
+																	<!-- More Actions Button -->
+																	<button
+																		class="flex rounded-md bg-gray-100 py-2 px-1 text-white
+        transition-all duration-150 ease-in-out hover:bg-gray-200"
+																	>
+																		<svg
+																			class="fill-gray-500"
+																			xmlns="http://www.w3.org/2000/svg"
+																			xmlns:xlink="http://www.w3.org/1999/xlink"
+																			version="1.1"
+																			width="24"
+																			height="24"
+																			viewBox="0 0 24 24"
+																		>
+																			<path
+																				d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z"
+																			/>
+																		</svg>
+																	</button>
+																</div>
+															</div>
+														</div></TableBodyCell
 													>
-												</TableBodyCell>
-											</TableBodyRow>{/if}
+												</TableBodyRow>
+											</div>
+										{/if}
 									{/each}
 								</TableBody>
 							</div>
